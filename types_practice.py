ba5aef84147a6ea2c175372245bfa1659c03c278
Shujie <s.j.zhu@outlook.com>
2024-01-28 13:35:19 +0000

    completed types_practice and created operations

---
+++ types_practice.py
@@ -0,0 +1,18 @@
+first_name = 'shujie'
+last_name = 'zhu'
+# my two variables are "first_name" and "last_name"
+# putting quotes around the word makes it a string
+
+full_name = first_name + ' ' + last_name
+# to put a space in the output, it will literally need to be a space in quotes
+print(full_name)
+
+# list - transfer the variables into a list
+name_list = [first_name, last_name]
+print(name_list)
+# lists are in square brackets
+
+# dictionary - transfer the variables into a dictionary
+name_dictionary = {'first': 'shujie', 'last':'zhu'}
+print(name_dictionary)
+# dictionary are in braces, and are presented in key: value pairs
