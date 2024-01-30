d01e6418e17254a79d69c4d3972ca951bee8aadc
Jiya Bharti <Jiyabharti@oulook.com>
2024-01-30 17:23:05 +0000

    combined the files

--- types_practice.py
+++ types_practice.py
@@ -1,3 +1,4 @@
+# Shujie's Name Dictionary
 first_name = 'shujie'
 last_name = 'zhu'
 # my two variables are "first_name" and "last_name"
@@ -16,3 +17,33 @@ print(name_list)
 name_dictionary = {'first': 'shujie', 'last': 'zhu'}
 print(name_dictionary)
 # dictionary are in braces, and are presented in key: value pairs
+
+
+# EXERCISE 9, Part 1
+# Two Variables below one containing my first name and the other my last
+first_name = 'Jiya'
+last_name = 'Bharti'
+# the print function and concatenation allows for the display with a space
+print(first_name + ' ' + last_name)
+
+# Now I am adding the variables into a list
+fullname_list = [first_name + ' ' + last_name]
+print(fullname_list)
+
+
+# Jiya's Name Dictionary
+                                    # DICTIONARY CREATION
+
+# creating a list using variables and keys
+# the keys and their corresponding values
+keys = ['first','last']
+values = [first_name, last_name]
+
+# Creating a dictionary using a loop
+my_dict = {}
+for i in range(len(keys)):
+    my_dict[keys[i]] = values[i]
+#     I have no clue what the above does tbh, I tried figuring it out..ASKKKK!!!!!!!
+
+# Displaying the dictionary
+print(my_dict)
