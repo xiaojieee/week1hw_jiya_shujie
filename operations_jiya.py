40fb62fa7c0b6cf0d90c2f1d97f846552147249a
Jiya Bharti <Jiyabharti@oulook.com>
2024-01-30 16:11:35 +0000

    Homework added to repo

---
+++ operations_jiya.py
@@ -0,0 +1,13 @@
+# EXERCISE 9, Part 2
+
+# ASK VICTORIA - everytime I used input and another print why did it not work at the same time?
+# the var is an easy way of outputting the string
+var = input("please enter a value: ").upper()
+
+
+# now we need to print it but the value of var in uppercase
+
+
+print('uppercase', var)
+
+# WHY IS IT NOT UPPERCASE!
